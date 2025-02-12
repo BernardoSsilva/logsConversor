aa0718da17bad7fb8cb0939024c8015abf2cd0e7
bernardo.silva <bernardo.silva@agpr5.net>
2025-02-12 17:19:50 -0300

    test basic code

---
+++ LogsConversor/Program.cs
@@ -0,0 +1,34 @@
+﻿using LogsConversor.classes;
+
+class Program
+{
+
+
+    static void Main(string[] args) {
+        Console.WriteLine("Insert txt file path");
+        string filePath = Console.ReadLine();
+
+        if (!File.Exists($@"{filePath}\logs.txt"))
+        {
+            Console.WriteLine("File don't exists");
+            return;
+        }
+
+        string[] fileLines = File.ReadAllLines($@"{filePath}\logs.txt");
+
+        Conversor conversorInstance = new Conversor();
+
+        List<string> convertedLines = conversorInstance.convertLogs(fileLines);
+
+        Console.WriteLine("Insert path to save the parsed log file");
+        string pathToSave = Console.ReadLine();
+
+        conversorInstance.createFile(pathToSave, convertedLines);
+
+    }
+
+
+
+
+
+}
---
+++ LogsConversor/classes/conversor.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace LogsConversor.classes
+{
+    public class Conversor
+    {
+        public List<string> convertLogs(string[] logLines)
+        {
+
+
+            List<string> result = [];
+
+            for (int i = 0; i < logLines.Length; i++)
+            {
+
+                //responseSize,statusCode, cacheStatus,uriWithMethod, timeTaken
+                string[] data = logLines[i].Split("|");
+                string responseSize = data[0];
+                string statusCode = data[1];
+                string cacheStatus = data[2];
+
+                // method, path
+                string[] uriData = data[3].Split('/', ' ');
+                string method = uriData[0].Replace('\"', ' ');
+
+
+                string path = "/" + uriData[1];
+
+                string timeTaken = data[4];
+
+                string parsedLine = $@"{method} {statusCode} {path} {responseSize} {timeTaken} {cacheStatus}";
+
+
+                result.Add(parsedLine);
+            }
+
+            return result;
+        }
+
+
+        public void createFile(string path, List<string> parsedLines)
+        {
+            try
+            {
+
+                Directory.CreateDirectory(Path.GetDirectoryName(path));
+
+                using (StreamWriter sw = new StreamWriter($@"C:\temp\logs\parsed.txt"))
+                {
+                    foreach(string parsedLine in parsedLines)
+                    {
+                        Console.WriteLine(parsedLine);
+                        sw.WriteLine(parsedLine);
+                    }
+                }
+            } catch
+            {
+                Console.WriteLine("error");
+            }
+           
+        }
+    }
+}
